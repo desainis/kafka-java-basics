f55555894a83b3941f21df8e780cc99ea966844a
Nishant Desai <nishant.desai@mail.utoronto.ca>
2020-01-05 12:29:24 -0500

    added consumer producer basics

--- src/main/java/com/github/desainis/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/desainis/kafka/tutorial1/ProducerDemo.java
@@ -1,6 +1,9 @@
 package com.github.desainis.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
@@ -12,8 +15,20 @@ public class ProducerDemo {
         // create Producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+
+        // send the data - asynchronous
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
     }
 }
